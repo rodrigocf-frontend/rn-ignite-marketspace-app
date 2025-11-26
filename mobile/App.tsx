568599057b1b851969cc06780aac089f264b751f
Rodrigo Cunha <rodrigoc.frontend@gmail.com>
2025-11-26 19:20:42 -0300

    feat: add bootstrap hook on application hook

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,19 +1,24 @@
 import "./global.css";
-import { GluestackUIProvider } from "@components/ui/gluestack-ui-provider";
-import { Center } from "@/components/ui/center";
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { GluestackUIProvider } from "@/config/gluestack-ui-provider";
+import { useBootstrap } from "@/hooks/useBootstrap";
+import { Login } from "@/screens/Login";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const { isReady } = useBootstrap();
+
+  useEffect(() => {
+    if (isReady) {
+      SplashScreen.hideAsync();
+    }
+  }, [isReady]);
+
   return (
     <GluestackUIProvider mode="light">
-      <SafeAreaView className="flex-1">
-        <Center className="flex-1 bg-primary-500 ">
-          <Text className="text-typography-0 font-bold">
-            This is the center.
-          </Text>
-        </Center>
-      </SafeAreaView>
+      <Login />
     </GluestackUIProvider>
   );
 }
