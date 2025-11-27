b46dd4421e79188ee585b06aca795d6bdfe9e398
Rodrigo Cunha <rodrigoc.frontend@gmail.com>
2025-11-27 16:52:02 -0300

    style: add new fontsize

--- mobile/tailwind.config.js
+++ mobile/tailwind.config.js
@@ -175,7 +175,9 @@ module.exports = {
       fontWeight: {
         extrablack: "950",
       },
-      fontSize: {},
+      fontSize: {
+        "2xs": "10px",
+      },
 
       boxShadow: {
         "hard-1": "-2px 2px 8px 0px rgba(38, 38, 38, 0.20)",
